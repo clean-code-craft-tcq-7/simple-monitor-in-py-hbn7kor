6ec21d4a89e09af1b7efbbcdbea8ea2f4b6f6d53
hbn7kor <hbn7kor@bosch.com>
2025-07-18 15:32:30 +0530

    updating the scripts in line with the workflows

--- monitor.py
+++ monitor.py
@@ -1,37 +1,38 @@
-
-from time import sleep
 import sys
+from time import sleep
 
 
-def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
+def display(message):
+    print(message)
     for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
+        print("\r* ", end="")
+        sys.stdout.flush()
+        sleep(1)
+        print("\r *", end="")
+        sys.stdout.flush()
+        sleep(1)
     return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+
+
+def is_inrange(value, min_value, max_value):
+    return min_value < value < max_value
+
+
+def check_vitals(value, min_value, max_value, message):
+    return True if is_inrange(value, min_value, max_value) else display(message)
+
+
+def check_temp(temperature):
+    return check_vitals(temperature, 95, 102, "Temperature critical!")
+
+
+def check_pulse(pulseRate):
+    return check_vitals(pulseRate, 60, 100, "Pulse Rate is out of range!")
+
+
+def check_spo2(spo2):
+    return check_vitals(spo2, 90, 100, "Oxygen Saturation out of range!")
+
+
+def vitals_ok(temperature, pulse, spo2):
+    return check_temp(temperature) and check_pulse(pulse) and check_spo2(spo2)
--- monitor.test.py
+++ monitor.test.py
@@ -1,12 +1,20 @@
 import unittest
-from monitor import vitals_ok
+from monitor import is_inrange, check_vitals, vitals_ok
 
 
 class MonitorTest(unittest.TestCase):
-    def test_not_ok_when_any_vital_out_of_range(self):
-        self.assertFalse(vitals_ok(99, 102, 70))
+    def test_vitals_ok(self):
         self.assertTrue(vitals_ok(98.1, 70, 98))
+        self.assertFalse(vitals_ok(99, 102, 70))
+
+    def test_is_in_range(self):
+        self.assertTrue(is_inrange(96, 95, 102))
+        self.assertFalse(is_inrange(110, 95, 102))
+
+    def test_check_vitals(self):
+        self.assertTrue(check_vitals(96, 95, 102, "sample error message!"))
+        self.assertFalse(check_vitals(110, 95, 102, "sample error message!"))
 
 
-if __name__ == '__main__':
-  unittest.main()
+if __name__ == "__main__":
+    unittest.main()
