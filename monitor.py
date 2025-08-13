df2332ca0a4dc93f88cef383819cceed9ba1038a
Bharathi Kannan Natarajan <hbn7kor@bosch.com>
2025-08-13 16:51:39 +0530

    adding additional vital parameters and language translation extension

--- monitor.py
+++ monitor.py
@@ -1,38 +1,49 @@
 import sys
-from time import sleep
+import time
+import random
+import deep_translator
+
+LANGUAGE = "german"
+VITALS = [
+    {"name": "temperature", "minvalue": 95, "maxvalue": 102, "message": "Temperature critical!"},
+    {"name": "pulserate", "minvalue": 60, "maxvalue": 100, "message": "Pulse Rate is out of range!"},
+    {"name": "spo2", "minvalue": 90, "maxvalue": 100, "message": "Oxygen Saturation out of range!"},
+    {"name": "blood-sugar", "minvalue": 70, "maxvalue": 110, "message": "Blood sugar levels out of range!"},
+    {"name": "blood-pressure", "minvalue": 90, "maxvalue": 150, "message": "Blood pressure levels out of range!"},
+    {"name": "respiratory-rate", "minvalue": 12, "maxvalue": 20, "message": "Blood sugar levels out of range!"},
+]
 
 
 def display(message):
-    print(message)
+    print(translate(message, LANGUAGE))
     for i in range(6):
         print("\r* ", end="")
         sys.stdout.flush()
-        sleep(1)
+        time.sleep(1)
         print("\r *", end="")
         sys.stdout.flush()
-        sleep(1)
+        time.sleep(1)
     return False
 
 
+def sensorStub():
+    return {"temperature": random.choice(range(95, 102)), "pulserate": random.choice(range(60, 100)), "spo2": random.choice(range(90, 100)), "blood-sugar": random.choice(range(70, 110)), "blood-pressure": random.choice(range(90, 150)), "respiratory-rate": random.choice(range(12, 20))}
+
+
+def translate(text, target):
+    return deep_translator.GoogleTranslator(target=target).translate(text)
+
+
 def is_inrange(value, min_value, max_value):
-    return min_value < value < max_value
+    return min_value <= value <= max_value
 
 
 def check_vitals(value, min_value, max_value, message):
     return True if is_inrange(value, min_value, max_value) else display(message)
 
 
-def check_temp(temperature):
-    return check_vitals(temperature, 95, 102, "Temperature critical!")
-
-
-def check_pulse(pulseRate):
-    return check_vitals(pulseRate, 60, 100, "Pulse Rate is out of range!")
-
-
-def check_spo2(spo2):
-    return check_vitals(spo2, 90, 100, "Oxygen Saturation out of range!")
-
-
-def vitals_ok(temperature, pulse, spo2):
-    return check_temp(temperature) and check_pulse(pulse) and check_spo2(spo2)
+def vitals_ok(sensorstub):
+    for obj in VITALS:
+        if not check_vitals(sensorstub[obj["name"]], obj["minvalue"], obj["maxvalue"], obj["message"]):
+            return False
+    return True
--- monitor.test.py
+++ monitor.test.py
@@ -1,11 +1,16 @@
 import unittest
-from monitor import is_inrange, check_vitals, vitals_ok
+from monitor import display, sensorStub, translate, is_inrange, check_vitals, vitals_ok
 
 
 class MonitorTest(unittest.TestCase):
-    def test_vitals_ok(self):
-        self.assertTrue(vitals_ok(98.1, 70, 98))
-        self.assertFalse(vitals_ok(99, 102, 70))
+    def test_display(self):
+        self.assertFalse(display("sample error message"))
+
+    def test_sensorStub(self):
+        self.assertIsNotNone(sensorStub())
+
+    def test_translate(self):
+        self.assertEqual(translate("Good Morning", "german"), "Guten Morgen")
 
     def test_is_in_range(self):
         self.assertTrue(is_inrange(96, 95, 102))
@@ -14,6 +19,39 @@ class MonitorTest(unittest.TestCase):
     def test_check_vitals(self):
         self.assertTrue(check_vitals(96, 95, 102, "sample error message!"))
         self.assertFalse(check_vitals(110, 95, 102, "sample error message!"))
+
+    def test_vitals_ok(self):
+        self.assertTrue(vitals_ok(sensorStub()))
+
+    def test_temperature(self):
+        sensorstub = sensorStub()
+        sensorstub["temperature"] = 103
+        self.assertFalse(vitals_ok(sensorstub))
+
+    def test_pulserate(self):
+        sensorstub = sensorStub()
+        sensorstub["pulserate"] = 103
+        self.assertFalse(vitals_ok(sensorstub))
+
+    def test_spo2(self):
+        sensorstub = sensorStub()
+        sensorstub["spo2"] = 103
+        self.assertFalse(vitals_ok(sensorstub))
+
+    def test_blood_sugar(self):
+        sensorstub = sensorStub()
+        sensorstub["blood-sugar"] = 112
+        self.assertFalse(vitals_ok(sensorstub))
+
+    def test_blood_pressure(self):
+        sensorstub = sensorStub()
+        sensorstub["blood-pressure"] = 160
+        self.assertFalse(vitals_ok(sensorstub))
+
+    def test_respiratory_rate(self):
+        sensorstub = sensorStub()
+        sensorstub["respiratory-rate"] = 25
+        self.assertFalse(vitals_ok(sensorstub))
 
 
 if __name__ == "__main__":
